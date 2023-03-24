32d1dc8176ff5c1270fbb7cd6d88c53508601813
Steph Nutzel <stephnutzelgit@gmail.com>
2023-03-24 16:03:09 +0100

    added seup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='cratemain',
+    version='0.1',
+    description='App controller',
+    author='Steph Nützel',
+    author_email='stephnutzelgit@gmail.com',
+    packages=['cratemain'],  # same as name
+)
